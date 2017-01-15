e316027f2941cc040fa3e599ba1a6d4ccaf0b424
Rebecca Franks <franks.rebecca@gmail.com>
2017-01-15 15:29:38 +0200

    Added Android Things Blinking LED project.

---
+++ app/src/main/java/za/co/riggaroo/blinkingled/BlinkingActivity.java
@@ -0,0 +1,72 @@
+package za.co.riggaroo.blinkingled;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+
+public class BlinkingActivity extends Activity {
+
+    private static final String TAG = "BlinkActivity";
+    private static final int INTERVAL_BETWEEN_BLINKS_MS = 1000;
+
+    private Handler blinkingLedHandler = new Handler();
+
+    private Gpio ledGpio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blinking);
+
+        PeripheralManagerService service = new PeripheralManagerService();
+        try {
+            String gpioPinName = BoardDefaults.getGPIOForLED();
+            ledGpio = service.openGpio(gpioPinName);
+
+            ledGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+
+            blinkingLedHandler.post(blinkingRunnable);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        blinkingLedHandler.removeCallbacks(blinkingRunnable);
+
+        if (ledGpio != null) {
+            try {
+                ledGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
+    }
+
+    private Runnable blinkingRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (ledGpio == null) {
+                return;
+            }
+
+            try {
+                ledGpio.setValue(!ledGpio.getValue());
+
+                blinkingLedHandler.postDelayed(blinkingRunnable, INTERVAL_BETWEEN_BLINKS_MS);
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
+    };
+
+}
